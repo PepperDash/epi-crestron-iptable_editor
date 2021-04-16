15610a1ba3ec57c5eb2a3d53d1fe0df3a237b361
Trevor Payne <tpayne@pepperdash.com>
2021-04-16 15:51:11 -0500

    Update Joinmap to EiscApiAdvanced
    Update plugin load methodology

---
+++ PDT.Utilities.IPTableEditor.EPI/JoinMap.cs
@@ -0,0 +1,29 @@
+﻿using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Bridges;
+
+namespace IPTableEditorEPI
+{
+    public class IpTableEditorJoinMap : DisplayControllerJoinMap
+    {
+        /// <summary>
+        /// Analog join to report LED product monitor temperature feedback
+        /// </summary>
+        [JoinName("CheckTable")]
+        public JoinDataComplete CheckTable =
+            new JoinDataComplete(new JoinData {JoinNumber = 1, JoinSpan = 1},
+                new JoinMetadata
+                {
+                    Description = "Show if table needs checked, and check table",
+                    JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                    JoinType = eJoinType.Digital
+                });
+
+        public IpTableEditorJoinMap(uint joinStart)
+            : base(joinStart, typeof(IpTableEditorJoinMap))
+		{
+        }
+
+
+    }
+
+}
