b455feee8f783d82ca29e06993976847525aca49
Trevor Payne <44117050+TrevorPayne@users.noreply.github.com>
2021-04-16 15:33:40 -0500

    Updated AssemblyInfo.cs

--- PDT.Utilities.IPTableEditor.EPI/Properties/AssemblyInfo.cs
+++ PDT.Utilities.IPTableEditor.EPI/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@
 [assembly: AssemblyTitle("IPTableEditorTemplateEpi")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("IPTableEditorTemplateEpi")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyVersion("1.0.0.*")]
-
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0-buildtype-buildnumber")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("0.0.0-buildtype-buildnumber")]
