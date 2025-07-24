9bee5ed73d6f53241263d0890df48430c4ad96d3
Jonathan Arndt <jarndt@pepperdash.com>
2025-07-24 12:27:55 -0700

    chore(docs): add XML documentation to all public members with <inheritdoc/> where appropriate

---
+++ IpTableEditorPlugin/IPTableEditorBridgeJoinMap.cs
@@ -0,0 +1,92 @@
+﻿using PepperDash.Essentials.Core;
+
+namespace IPTableEditorPlugin 
+{
+    public class IpTableEditorBridgeJoinMap : JoinMapBaseAdvanced
+    {
+        /// <summary>
+        /// Analog join to report LED product monitor temperature feedback
+        /// </summary>
+        [JoinName("CheckTable")]
+        public JoinDataComplete CheckTable = new JoinDataComplete(
+                new JoinData
+                {
+                    JoinNumber = 1, 
+                    JoinSpan = 10
+                },
+                new JoinMetadata
+                {
+                    Description = "Feedback showing if the table needs to be checked, and input to check the table.",
+                    JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                    JoinType = eJoinType.Digital
+                });
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="joinStart"></param>
+        public IpTableEditorBridgeJoinMap(uint joinStart)
+            : base(joinStart, typeof(IpTableEditorBridgeJoinMap))
+        {
+        }
+    }
+
+    public class IpTableSelectorBridgeJoinMap : JoinMapBaseAdvanced
+    {
+        /// <summary>
+        /// Analog join to report LED product monitor temperature feedback
+        /// </summary>
+        [JoinName("SelectItemBool")]
+        public JoinDataComplete SelectItemBool = new JoinDataComplete(
+                new JoinData
+                {
+                    JoinNumber = 1,
+                    JoinSpan = 10
+                },
+                new JoinMetadata
+                {
+                    Description = "Select and Feedback for selecting each mutable IPTable Entry",
+                    JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                    JoinType = eJoinType.Digital
+                });
+
+
+        /// <summary>
+        /// Select and Feedback for selecting each mutable IPTable Entry (Analog join)
+        /// </summary>
+        [JoinName("SelectItemAnalog")]
+        public JoinDataComplete SelectItemAnalog = new JoinDataComplete(
+                new JoinData
+                {
+                    JoinNumber = 1,
+                    JoinSpan = 1
+                },
+                new JoinMetadata
+                {
+                    Description = "Select and Feedback for selecting each mutable IPTable Entry",
+                    JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                    JoinType = eJoinType.Analog
+                });
+
+        /// <inheritdoc/>
+        /// <param name="joinStart">The starting join number for the join map.</param>
+        /// <param name="ipIdCount">The number of IP table entries to map.</param>
+        public IpTableSelectorBridgeJoinMap(uint joinStart, int ipIdCount)
+            : base(joinStart, typeof(IpTableSelectorBridgeJoinMap))
+        {
+            SelectItemBool = new JoinDataComplete(
+                new JoinData
+                {
+                    JoinNumber = 1,
+                    JoinSpan = (uint)ipIdCount
+                },
+                new JoinMetadata
+                {
+                    Description = "Select and Feedback for selecting each mutable IPTable Entry",
+                    JoinCapabilities = eJoinCapabilities.ToFromSIMPL,
+                    JoinType = eJoinType.Digital
+                });
+        }
+    }
+
+}
---
+++ IpTableEditorPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("IPTableEditorTemplateEpi")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IPTableEditorTemplateEpi")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyVersion("0.0.0.*")]
+[assembly: AssemblyInformationalVersion("0.0.0-buildtype-buildnumber")]
+[assembly: Crestron.SimplSharp.Reflection.AssemblyInformationalVersion("0.0.0-buildtype-buildnumber")]
